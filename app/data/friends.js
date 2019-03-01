9dcdf0c0718c610a77c7173e3e57d5ed98e26579
Kaleigh Herold <kaleigh224@gmail.com>
2019-03-01 12:19:44 -0500

    added modal and also got it to save answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,40 @@
-// Save application's data to an array
+// Save application's data to an array - fill with two opposite users (dummy data)
 
 // Ex:
-// {
-//     "name":"Ahmed",
-//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//     "scores":[
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-//   }
-  
+
+var friends = [
+{
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+      name: "Liz",
+      photo: "https://www.google.com",
+      scores: [
+          5,
+          5,
+          4,
+          3,
+          3,
+          4,
+          5,
+          4,
+          2,
+          4
+      ]
+  }
+]
+
+module.exports = friends
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,34 @@
 
-module.exports = function(app) {
+module.exports = function (app) {
     var friends = require("../data/friends.js")
-// GET route with url /api/friends
-// JSON to display all possible friends
+    // GET route with url /api/friends
+    // JSON to display all possible friends
 
-app.get("/api/friends", function(req, res) {
-    return res.json(friends)
-})
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends)
+    })
 
 
-// POST route to /api/friends to handle incoming survey results
-// This route will handle the compatibility logic
+    // POST route to /api/friends to handle incoming survey results
+    // This route will handle the compatibility logic
 
-app.post("/api/friends", function(req, res) {
-    var compatibility = req.body
-    console.log(compatibility)
-    // "".push(compatibility);
-    res.json(compatibility)
-})
+    app.post("/api/friends", function (req, res) {
+        var compatibility = req.body
+        var smallestDifference = 50;
+        var bestMatch = {};
+        console.log(compatibility)
+        friends.forEach(function (friend) {
+            var totalDifference = 0;
+            console.log(friend.scores[0])
+            for (var i = 0; i < friend.scores.length; i++) {
+                totalDifference += Math.abs(friend.scores[i] - compatibility.scores[i])
+            }
+            if(totalDifference < smallestDifference) {
+                smallestDifference = totalDifference
+                bestMatch = friend
+            } 
+        })
+        res.json(bestMatch)
+        friends.push(compatibility)
+    })
 };
